a7f5d4dda978fd3a5fb960dfe6490e1c232fc606
Karl Zawisla <karl.zawisla@gmail.com>
2017-03-18 20:12:00 +0100

    nothing

--- IntelPlayer/Morceaux.cs
+++ IntelPlayer/Morceaux.cs
@@ -1,15 +1,27 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
 namespace IntelPlayer
 {
+    [Serializable]//indique qu'une classe est sérialisable
     class Morceaux
     {
-        private List<int> ch = new List<int>();
+        public string type { get; set;  }
+        public int number { get; set; }
+        public bool Healthy { get; set; }
 
-        //https://www.dotnetperls.com/serialize-list
+        public Morceaux(string t, int n, bool h)
+        {
+            this.type = t;
+            this.number = n;
+            this.Healthy = h;
+        }
     }
 }
